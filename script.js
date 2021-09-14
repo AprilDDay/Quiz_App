0b208d2c17104e7c98221ce4d31ba25805888e2b
April Day <april.day@gmail.com>
2021-09-14 15:03:05 +0100

    add script w/o any questions or answers

---
+++ script.js
@@ -0,0 +1,100 @@
+const quizData = [
+    {
+        question: "?",
+        a: "",
+        b: "",
+        c: "",
+        d: "",
+        correct: "d",
+    },
+    {
+        question: "?",
+        a: "",
+        b: "",
+        c: "",
+        d: "",
+        correct: "d",
+    },
+    {
+        question: "?",
+        a: "",
+        b: "",
+        c: "",
+        d: "",
+        correct: "d",
+    },
+    {
+        question: "?",
+        a: "",
+        b: "",
+        c: "",
+        d: "",
+        correct: "d",
+    },
+];
+
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+
+function loadQuiz(){
+    deselectAnswer();
+
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+
+}
+
+function getSelected(){
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if(answerEl.checked){
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswer(){
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
+submitBtn.addEventListener("click", ()=> {
+    //check answer
+    const answer = getSelection();
+
+    if(answer){
+        if (answer === quizData[currentQuiz].correct){
+            score++;
+        }
+
+        currentQuiz++;
+
+        if (currentQuiz < quizData.length) {
+
+            loadQuiz();
+
+        } else {
+            quiz.innerHTML = `<h2>Your score ${score}/${quizData.length}</h2>`;     
+        }
+    }
+});
+
+//insta @codecrooksshanks 
